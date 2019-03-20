4217a0396bdc23e1284ba4cc53deac1df82ebd0d
Greg Smalter <gsmalter@brossgroup.com>
2019-03-20 15:01:34 -0400

    Refactored EwlStatics into more specific classes. Deleted methods that were too specific to be useful in this library, or whose implementation may no longer be necessary given new language features.

---
+++ Tewl/Tools/ActionTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Tewl.Tools {
+	/// <summary>
+	/// Convenience methods to support Action objects.
+	/// </summary>
+	public static class ActionTools {
+		/// <summary>
+		/// Retries the given action until it executes without exception or maxAttempts is reached. You can specify different maxAttempts or retry intervals - the default is 30 tries
+		/// with a 2 second wait in between each try.
+		/// If every attempt fails, a new application exception will be thrown with the given message. The original exception will be the inner exception.
+		/// </summary>
+		public static void Retry( this Action action, string failureMessage, int maxAttempts = 30, int retryIntervalMs = 2000 ) {
+			for( var i = 0;; i += 1 ) {
+				try {
+					action();
+					break;
+				}
+				catch( Exception e ) {
+					if( i < maxAttempts )
+						Thread.Sleep( retryIntervalMs );
+					else
+						throw new ApplicationException( failureMessage, e );
+				}
+			}
+		}
+	}
+}
---
+++ Tewl/Tools/NetTools.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Tewl.Tools {
+	/// <summary>
+	/// Tools supporting DNS, URIs, encoding, and so forth.
+	/// </summary>
+	public class NetTools {
+		/// <summary>
+		/// Returns the host name of the local computer.
+		/// </summary>
+		public static string GetLocalHostName() {
+			return Dns.GetHostEntry( "" ).HostName;
+		}
+	}
+}
