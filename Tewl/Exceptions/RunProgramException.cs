d5a64d8aa756c0fd7f8b3e107ace2208845826a6
Sam Rueby <samrueby@gmail.com>
2019-03-21 10:40:00 -0400

    Improved ProcessTools.RunProgram.

---
+++ Tewl/Exceptions/RunProgramException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Tewl.Exceptions {
+	/// <inheritdoc />
+	/// <summary>
+	/// Thrown when there is an error running a program.
+	/// </summary>
+	public class RunProgramException: Exception {
+		public RunProgramException( int? errorCode, string message, string processOutput, string processError, Exception innerException = null ): base(
+			getMessage( errorCode, message, processOutput, processError ),
+			innerException ) { }
+
+		private static string getMessage( int? errorCode, string message, string processOutput, string processError ) {
+			try {
+				var sb = new StringBuilder();
+				sb.AppendLine( message );
+				if( errorCode != null )
+					sb.AppendLine( "Error code: " + errorCode );
+				sb.AppendLine( "Process Output: " + processOutput );
+				sb.AppendLine( "Process Error Output: " + processError );
+
+				return sb.ToString();
+			}
+			catch {
+				// Could fail with OutOfMemoryException
+				return "";
+			}
+		}
+	}
+}
