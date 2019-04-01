d0ea7658ae4083b693e8293ca50325e00e9f8280
Sam Rueby <samrueby@gmail.com>
2019-04-01 16:17:34 -0400

    Now possible to pass your own output handler when running a program.

--- Tewl/Exceptions/RunProgramException.cs
+++ Tewl/Exceptions/RunProgramException.cs
@@ -7,26 +7,34 @@ namespace Tewl.Exceptions {
 	/// Thrown when there is an error running a program.
 	/// </summary>
 	public class RunProgramException: Exception {
-		public RunProgramException( int? errorCode, string message, string processOutput, string processError, Exception innerException = null ): base(
-			getMessage( errorCode, message, processOutput, processError ),
-			innerException ) { }
+		private readonly string message;
+		private readonly int? errorCode;
+		internal string ProcessOutput;
+		internal string ProcessError;
 
-		private static string getMessage( int? errorCode, string message, string processOutput, string processError ) {
-			try {
-				var sb = new StringBuilder();
-				sb.AppendLine( message );
-				if( errorCode != null )
-					sb.AppendLine( "Error code: " + errorCode );
-				if( processOutput != null )
-					sb.AppendLine( "Process Output: " + processOutput );
-				if( processError != null )
-					sb.AppendLine( "Process Error Output: " + processError );
+		public RunProgramException( string message, int? errorCode, Exception innerException = null ):base(null, innerException) {
+			this.message = message;
+			this.errorCode = errorCode;
+		}
 
-				return sb.ToString();
-			}
-			catch {
-				// Could fail with OutOfMemoryException
-				return "";
+		public override string Message {
+			get {
+				try {
+					var sb = new StringBuilder();
+					sb.AppendLine( message );
+					if( errorCode != null )
+						sb.AppendLine( "Error code: " + errorCode );
+					if( ProcessOutput != null )
+						sb.AppendLine( "Process Output: " + ProcessOutput );
+					if( ProcessError != null )
+						sb.AppendLine( "Process Error Output: " + ProcessError );
+
+					return sb.ToString();
+				}
+				catch {
+					// Could fail with OutOfMemoryException
+					return "";
+				}
 			}
 		}
 	}
