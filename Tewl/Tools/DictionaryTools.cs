6809c7d310933cab7884395d571c0ca1a9c9724d
Sam Rueby <samrueby@gmail.com>
2020-02-13 15:26:58 -0500

    Migrated DictionaryTools from DeveloperTools.

--- Tewl/Tools/DictionaryTools.cs
+++ Tewl/Tools/DictionaryTools.cs
@@ -11,13 +11,13 @@ namespace Tewl.Tools {
 		/// Tries to get dictionary value. Returns null if the <paramref name="key" /> was not found.
 		/// Note that this only makes sense if you don't expect to receive null for a key found in the dictionary.
 		/// </summary>
-		public static V TryGetValue<K, V>( this Dictionary<K, V> dic, K key ) where V: class => dic.TryGetValue( key, out var v ) ? v : null;
+		public static V? TryGetValueNullableClass<K, V>( this Dictionary<K, V> dic, K key ) where V: class => dic.TryGetValue( key, out var v ) ? v : null;
 
 		/// <summary>
 		/// Tries to get dictionary value. Returns null if the <paramref name="key" /> was not found.
 		/// Note that this only makes sense if you don't expect to receive null for a key found in the dictionary.
 		/// </summary>
-		public static V? TryGetValueNullable<K, V>( this Dictionary<K, V> dic, K key ) where V: struct => dic.TryGetValue( key, out var v ) ? v : (V?)null;
+		public static V? TryGetValueNullableStruct<K, V>( this Dictionary<K, V> dic, K key ) where V: struct => dic.TryGetValue( key, out var v ) ? v : (V?)null;
 
 		/// <summary>
 		/// Tries to get dictionary value. Returns <paramref name="valueIfNotFound" /> if the <paramref name="key" /> was not
