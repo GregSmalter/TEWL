e48579d90b71e27ec6ced7e4b2aaf48de35532f2
Sam Rueby <samrueby@gmail.com>
2022-07-29 14:12:45 -0400

    AssemblyTools.BuildSingletonDictionary will now give a helpful exception message when the required property isn't found.

--- Tewl/Tools/AssemblyTools.cs
+++ Tewl/Tools/AssemblyTools.cs
@@ -16,16 +16,21 @@ namespace Tewl.Tools {
 		}
 
 		/// <summary>
-		/// Builds a dictionary containing all singletons in the specified assembly that implement the specified interface type. All singletons must implement an
+		/// Builds a dictionary containing all singletons in the specified assembly that implement the specified interface type.
+		/// All singletons must implement an
 		/// Instance property.
 		/// </summary>
 		public static Dictionary<TKey, TInterface> BuildSingletonDictionary<TInterface, TKey>( Assembly assembly, Func<TInterface, TKey> getIdMethod ) {
 			var singletons = new Dictionary<TKey, TInterface>();
 			foreach( var type in assembly.getImplementations<TInterface>() ) {
-				var instanceProperty = type.GetProperty( "Instance" );
+				var instancePropertyName = "Instance";
+				var instanceProperty = type.GetProperty( instancePropertyName );
+				if( instanceProperty is null )
+					throw new Exception( $"Unable to find required property '{instancePropertyName}' on type {type.FullName}." );
 				var singleton = (TInterface)instanceProperty.GetValue( null, null );
 				singletons.Add( getIdMethod( singleton ), singleton );
 			}
+
 			return singletons;
 		}
 
