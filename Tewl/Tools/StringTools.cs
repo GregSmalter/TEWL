cfd08d37aaaa64d76302cd07858770a0d390f855
William Gross <william@wgross.net>
2024-10-30 13:06:01 -0400

    Corrected MatchesSearch behavior

--- Tewl/Tools/StringTools.cs
+++ Tewl/Tools/StringTools.cs
@@ -414,13 +414,13 @@ public static class StringTools {
 	}
 
 	/// <summary>
-	/// Returns true if this string matches any of the given search terms.
+	/// Returns true if this string matches the given search terms.
 	/// </summary>
 	/// <param name="text"></param>
 	/// <param name="searchTerms"></param>
 	public static bool MatchesSearch( this string text, string searchTerms ) {
 		var terms = searchTerms.Separate();
-		return terms.Any( term => text.IsLike( term, ignoreSurroundingWhitespace: false ) );
+		return terms.All( term => text.IsLike( term, ignoreSurroundingWhitespace: false ) );
 	}
 
 	/// <summary>
