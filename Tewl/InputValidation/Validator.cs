3167f840a985a293835516c01910a138760d149c
William Gross <william@wgross.net>
2025-03-21 11:24:16 -0400

    Cleaned up Validator

--- Tewl/InputValidation/Validator.cs
+++ Tewl/InputValidation/Validator.cs
@@ -10,14 +10,12 @@
 [ PublicAPI ]
 public class Validator {
 	/// <summary>
-	/// Most of our SQL Server decimal columns are specified as (9,2). This is the minimum value that will fit in such a
-	/// column.
+	/// Most of our SQL Server decimal columns are specified as (9,2). This is the minimum value that will fit in such a column.
 	/// </summary>
 	public const decimal SqlDecimalDefaultMin = -9999999.99m;
 
 	/// <summary>
-	/// Most of our SQL Server decimal columns are specified as (9,2). This is the maximum value that will fit in such a
-	/// column.
+	/// Most of our SQL Server decimal columns are specified as (9,2). This is the maximum value that will fit in such a column.
 	/// </summary>
 	public const decimal SqlDecimalDefaultMax = 9999999.99m;
 
@@ -36,52 +34,32 @@ public class Validator {
 		return input is null;
 	}
 
-	private readonly List<Error> errors = new List<Error>();
-
 	/// <summary>
-	/// Returns true if any errors have been encountered during validation so far.  This can be true even while
-	/// ErrorMessages.Count == 0
-	/// and Errors.Count == 0, since NoteError may have been called.
+	/// Returns true if any errors have been encountered during validation so far. This can be true even while ErrorMessages.Count == 0 and Errors.Count == 0,
+	/// since NoteError may have been called.
 	/// </summary>
 	public bool ErrorsOccurred { get; private set; }
 
+	private readonly List<Error> errors = [ ];
+
 	/// <summary>
-	/// Returns true if at least one unusable value has been returned since this Validator was created.  An unusable value
-	/// return
-	/// is defined as any time a Get... fails validation and the Validator is forced to return something other than
-	/// a good default value.  An example of an unusable value would be a call to GetInt that fails validation.  An
-	/// example of a usable value is a call to GetNullableInt, with allowEmpty = true, that fails validation.
+	/// Returns true if at least one unusable value has been returned since this Validator was created. An unusable value return is defined as any time a Get…
+	/// fails validation and the Validator is forced to return something other than an allowed empty value. An example of an unusable value would be a call to
+	/// GetInt that fails validation. An example of a usable value is a call to GetNullableInt, with allowEmpty = true, that fails validation.
 	/// </summary>
-	public bool UnusableValuesReturned {
-		get {
-			foreach( var error in errors )
-				if( error.UnusableValueReturned )
-					return true;
-
-			return false;
-		}
-	}
+	public bool UnusableValuesReturned => errors.Any( i => i.UnusableValueReturned );
 
 	/// <summary>
-	/// Returns a deep copy of the list of error messages associated with the validation performed by this validator so far.
-	/// It's possible for ErrorsOccurred to
+	/// Returns a deep copy of the list of error messages associated with the validation performed by this validator so far. It’s possible for ErrorsOccurred to
 	/// be true while ErrorsMessages.Count == 0, since NoteError may have been called.
 	/// </summary>
-	public List<string> ErrorMessages {
-		get {
-			var errorMessages = new List<string>();
-			foreach( var error in errors )
-				errorMessages.Add( error.Message );
-			return errorMessages;
-		}
-	}
+	public IReadOnlyCollection<string> ErrorMessages => errors.Select( i => i.Message ).Materialize();
 
 	/// <summary>
-	/// Returns a deep copy of the list of errors associated with the validation performed by this validator so far. It's
-	/// possible for ErrorsOccurred to
-	/// be true while Errors.Count == 0, since NoteError may have been called.
+	/// Returns a deep copy of the list of errors associated with the validation performed by this validator so far. It’s possible for ErrorsOccurred to be true
+	/// while Errors.Count == 0, since NoteError may have been called.
 	/// </summary>
-	public List<Error> Errors => new List<Error>( errors );
+	public IReadOnlyCollection<Error> Errors => [ ..errors ];
 
 	/// <summary>
 	/// Sets the ErrorsOccurred flag.
@@ -89,9 +67,8 @@ public class Validator {
 	public void NoteError() => ErrorsOccurred = true;
 
 	/// <summary>
-	/// Sets the ErrorsOccurred flag and adds an error message to this validator. Use this if you want to add your own error
-	/// message to the same collection
-	/// that the error handlers use.
+	/// Sets the ErrorsOccurred flag and adds an error message to this validator. Use this if you want to add your own error message to the same collection that
+	/// the error handlers use.
 	/// </summary>
 	public void NoteErrorAndAddMessage( string message ) {
 		NoteError();
@@ -99,9 +76,8 @@ public class Validator {
 	}
 
 	/// <summary>
-	/// Sets the ErrorsOccurred flag and add the given error messages to this validator. Use this if you want to add your own
-	/// error messages to the same collection
-	/// that the error handlers use.
+	/// Sets the ErrorsOccurred flag and add the given error messages to this validator. Use this if you want to add your own error messages to the same
+	/// collection that the error handlers use.
 	/// </summary>
 	public void NoteErrorAndAddMessages( params string[] messages ) {
 		foreach( var message in messages )
