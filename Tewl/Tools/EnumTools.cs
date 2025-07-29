78f60178570c9ec9cfc3a25a4991020f3d94d726
William Gross <william@wgross.net>
2025-07-29 15:09:26 -0400

    Added type constraints to EnumTools methods
    Support for these was added in C# 7.3 in 2018.

--- Tewl/Tools/EnumTools.cs
+++ Tewl/Tools/EnumTools.cs
@@ -9,14 +9,12 @@ public static class EnumTools {
 	/// Converts this string to a given Enum value. Case sensitive.
 	/// This method does not enforce valid Enum values.
 	/// </summary>
-	/// C# doesn't allow constraining the value to an Enum
-	public static T ToEnum<T>( this string s ) => (T)Enum.Parse( typeof( T ), s );
+	public static T ToEnum<T>( this string s ) where T: Enum => (T)Enum.Parse( typeof( T ), s );
 
 	/// <summary>
 	/// Gets the values of the specified enumeration type.
 	/// </summary>
-	// C# doesn't allow constraining the type to an Enum.
-	public static IEnumerable<T> GetValues<T>() => Enum.GetValues( typeof( T ) ).Cast<T>();
+	public static IEnumerable<T> GetValues<T>() where T: Enum => Enum.GetValues( typeof( T ) ).Cast<T>();
 
 	/// <summary>
 	/// Looks for <see cref="EnglishAttribute" /> and if available, returns its value.
