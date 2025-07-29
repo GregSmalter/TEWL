48a8a4b08abf9d931ed7d41c1c416532219cf21e
William Gross <william@wgross.net>
2025-07-29 15:05:44 -0400

    Fixed nullability warnings in ReflectionTools

--- Tewl/Tools/ReflectionTools.cs
+++ Tewl/Tools/ReflectionTools.cs
@@ -16,8 +16,8 @@ public static class ReflectionTools {
 	/// Returns the attribute for the given object if it's available. Otherwise, returns null.
 	/// Improve performance by declaring your attribute as sealed.
 	/// </summary>
-	public static T GetAttribute<T>( this object e ) where T: Attribute {
-		var memberInfo = e.GetType().GetMember( e.ToString() );
+	public static T? GetAttribute<T>( this object e ) where T: Attribute {
+		var memberInfo = e.GetType().GetMember( e.ToString()! );
 		if( memberInfo.Any() ) {
 			var attributes = memberInfo[ 0 ].GetCustomAttributes( typeof( T ), false );
 			if( attributes.Any() )
