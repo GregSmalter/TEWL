3158bd8784c35078f3c90bb15b76403c201b3aab
William Gross <william@wgross.net>
2025-07-29 15:04:38 -0400

    Trivial: Formatted EnumTools

--- Tewl/Tools/EnumTools.cs
+++ Tewl/Tools/EnumTools.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using JetBrains.Annotations;
+﻿namespace Tewl.Tools;
 
-namespace Tewl.Tools {
+/// <summary>
+/// Extension methods and other static tools pertaining to Enum types.
+/// </summary>
+[ PublicAPI ]
+public static class EnumTools {
 	/// <summary>
-	/// Extension methods and other static tools pertaining to Enum types.
+	/// Converts this string to a given Enum value. Case sensitive.
+	/// This method does not enforce valid Enum values.
 	/// </summary>
-	[ PublicAPI ]
-	public static class EnumTools {
-		/// <summary>
-		/// Converts this string to a given Enum value. Case sensitive.
-		/// This method does not enforce valid Enum values.
-		/// </summary>
-		/// C# doesn't allow constraining the value to an Enum
-		public static T ToEnum<T>( this string s ) => (T)Enum.Parse( typeof( T ), s );
+	/// C# doesn't allow constraining the value to an Enum
+	public static T ToEnum<T>( this string s ) => (T)Enum.Parse( typeof( T ), s );
 
-		/// <summary>
-		/// Gets the values of the specified enumeration type.
-		/// </summary>
-		// C# doesn't allow constraining the type to an Enum.
-		public static IEnumerable<T> GetValues<T>() => Enum.GetValues( typeof( T ) ).Cast<T>();
+	/// <summary>
+	/// Gets the values of the specified enumeration type.
+	/// </summary>
+	// C# doesn't allow constraining the type to an Enum.
+	public static IEnumerable<T> GetValues<T>() => Enum.GetValues( typeof( T ) ).Cast<T>();
 
-		/// <summary>
-		/// Looks for <see cref="EnglishAttribute" /> and if available, returns its value.
-		/// Otherwise, returns the name of the enum value after applying <see cref="StringTools.CamelToEnglish(string)" />.
-		/// </summary>
-		public static string ToEnglish( this Enum e ) {
-			var name = e.GetAttribute<EnglishAttribute>();
-			return name != null ? name.English : Enum.GetName( e.GetType(), e ).CamelToEnglish();
-		}
+	/// <summary>
+	/// Looks for <see cref="EnglishAttribute" /> and if available, returns its value.
+	/// Otherwise, returns the name of the enum value after applying <see cref="StringTools.CamelToEnglish(string)" />.
+	/// </summary>
+	public static string ToEnglish( this Enum e ) {
+		var name = e.GetAttribute<EnglishAttribute>();
+		return name != null ? name.English : Enum.GetName( e.GetType(), e ).CamelToEnglish();
 	}
 }
