bff9ad7e4ef86a45f4bda0786a607c13733bea76
William Gross <william@wgross.net>
2025-07-30 12:30:58 -0400

    Expanded LocalDateTools with new methods
    Added IsBetween and MonthBeginDate.

--- Tewl/Tools/LocalDateTools.cs
+++ Tewl/Tools/LocalDateTools.cs
@@ -8,6 +8,25 @@ namespace Tewl.Tools;
 [ PublicAPI ]
 public static class LocalDateTools {
 	/// <summary>
+	/// Returns whether this date is between (inclusive) the specified dates. Passing null for either of the two dates is considered to be infinity in that
+	/// direction. Therefore, passing null for both dates will always return true.
+	/// </summary>
+	public static bool IsBetween( this LocalDate date, LocalDate? begin, LocalDate? end ) =>
+		new DateInterval( begin ?? LocalDate.MinIsoValue, end ?? LocalDate.MaxIsoValue ).Contains( date );
+
+	/// <summary>
+	/// Returns whether this date is between (inclusive) the specified dates. Passing null for either of the two dates is considered to be infinity in that
+	/// direction. Therefore, passing null for both dates will always return true.
+	/// </summary>
+	[ Obsolete( "Please use IsBetween instead, which has exactly the same behavior." ) ]
+	public static bool IsBetweenDates( this LocalDate date, LocalDate? begin, LocalDate? end ) => date.IsBetween( begin, end );
+
+	/// <summary>
+	/// Returns the first date in this date’s month.
+	/// </summary>
+	public static LocalDate MonthBeginDate( this LocalDate date ) => date.ToYearMonth().OnDayOfMonth( 1 );
+
+	/// <summary>
 	/// Formats this date in "day month year" style, e.g. 5 Apr 2008. Returns stringIfNull if the date is null.
 	/// </summary>
 	public static string ToDayMonthYearString( this LocalDate? date, string stringIfNull, bool useLeadingZero, bool includeDayOfWeek = false ) =>
