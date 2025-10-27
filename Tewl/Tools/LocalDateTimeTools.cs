26f91e4da1be978acb670fd529acc05e54c1ff84
William Gross <william@wgross.net>
2025-10-27 18:00:34 -0400

    Created LocalDateTimeTools

---
+++ Tewl/Tools/LocalDateTimeTools.cs
@@ -0,0 +1,16 @@
+﻿using NodaTime;
+
+namespace Tewl.Tools;
+
+/// <summary>
+/// LocalDateTime extensions.
+/// </summary>
+[ PublicAPI ]
+public static class LocalDateTimeTools {
+	/// <summary>
+	/// Returns whether this date/time is between the specified dates/times, inclusive at the beginning and exclusive at the end. Passing null for either of the
+	/// two endpoints is considered to be infinity in that direction. Therefore, passing null for both endpoints will always return true.
+	/// </summary>
+	public static bool IsBetween( this LocalDateTime dateAndTime, LocalDateTime? begin, LocalDateTime? end ) =>
+		( begin is not {} b || dateAndTime >= b ) && ( end is not {} e || dateAndTime < e );
+}
