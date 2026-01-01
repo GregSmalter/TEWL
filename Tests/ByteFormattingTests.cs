f528d7af3e20c7a57bca6c9ba56487d9bc36fa7a
William Gross <william@wgross.net>
2026-01-01 13:56:34 -0500

    Moved in ByteFormattingTests from EWL

---
+++ Tests/ByteFormattingTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests;
+
+class ByteFormattingTests {
+	[ Test ]
+	public void Bytes() {
+		Assert.That( FormattingMethods.GetFormattedBytes( 64 ), Is.EqualTo( "64 bytes" ) );
+	}
+
+	[ Test ]
+	public void Kilo() {
+		Assert.That( FormattingMethods.GetFormattedBytes( 64_000 ), Is.EqualTo( "62 KiB" ) );
+	}
+
+	[ Test ]
+	public void Mega() {
+		Assert.That( FormattingMethods.GetFormattedBytes( 64_000_000 ), Is.EqualTo( "61 MiB" ) );
+	}
+
+	[ Test ]
+	public void Giga() {
+		Assert.That( FormattingMethods.GetFormattedBytes( 64_500_000_000 ), Is.EqualTo( "60.1 GiB" ) );
+	}
+}
