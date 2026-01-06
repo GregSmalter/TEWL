b9e73e15b9afdfa12052760fa4ffc7fcc3dbbc15
William Gross <william@wgross.net>
2026-01-06 15:15:00 -0500

    Added GetJsonArray
    This is tested code moving in from EWL.

---
+++ Tests/InputValidation/JsonTests.cs
@@ -0,0 +1,30 @@
+﻿using Tewl.InputValidation;
+
+namespace Tests.InputValidation;
+
+class JsonTests {
+	[ Test ]
+	public void ArrayEmpty() {
+		Assert.That( new Validator().GetJsonArray( null, "" ).Error( out _ ), Is.Null );
+	}
+
+	[ Test ]
+	public void ArraySingleValue() {
+		Assert.That( new Validator().GetJsonArray( null, "1" ).Error( out _ ), Is.Null );
+	}
+
+	[ Test ]
+	public void ArrayTwoValues() {
+		Assert.That( new Validator().GetJsonArray( null, "1,2" ).Error( out _ ), Is.Null );
+	}
+
+	[ Test ]
+	public void ArrayTwoValuesSpaced() {
+		Assert.That( new Validator().GetJsonArray( null, "  1 ,   2 " ).Error( out _ ), Is.Null );
+	}
+
+	[ Test ]
+	public void ArrayTwoValuesTrailingComma() {
+		Assert.That( new Validator().GetJsonArray( null, "1,2," ).Error( out _ ), Is.Not.Null );
+	}
+}
---
+++ Tewl/InputValidation/Validation Methods/JSON.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Tewl.InputValidation;
+
+partial class ValidatorExtensions {
+	/// <summary>
+	/// Returns the JSON array represented by the specified input string.
+	/// </summary>
+	public static ValidationResult<JsonArray?> GetJsonArray( this Validator validator, ValidationErrorHandler? errorHandler, string input ) =>
+		validator.ExecuteValidation<JsonArray, string>(
+			errorHandler ?? new ValidationErrorHandler( "value(s)" ),
+			input,
+			true,
+			( valueSetter, trimmedInput ) => {
+				JsonArray value;
+				try {
+					value = (JsonArray)JsonNode.Parse( $"[{trimmedInput}]" )!;
+				}
+				catch( JsonException ) {
+					return ValidationError.Custom( ValidationErrorType.Invalid, "The {0} must be valid JSON." );
+				}
+
+				valueSetter( value );
+				return null;
+			} );
+}
