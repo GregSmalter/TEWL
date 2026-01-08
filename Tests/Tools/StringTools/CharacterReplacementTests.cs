7aabcb992a35ca163f9c40b74d69d809bb0cf039
William Gross <william@wgross.net>
2026-01-08 09:27:35 -0500

    Moved in CharacterReplacementTests from EWL

---
+++ Tests/Tools/StringTools/CharacterReplacementTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.Tools.StringTools;
+
+class CharacterReplacementTests {
+	[ Test ]
+	public void RemoveNonAlphanumericCharacters() {
+		Assert.That( "abcdefghijklmnopqrstuvxyz".RemoveNonAlphanumericCharacters(), Is.EqualTo( "abcdefghijklmnopqrstuvxyz" ) );
+		Assert.That( "ABCDEFGHIJKLMNOPQRSTUVXYZ".RemoveNonAlphanumericCharacters(), Is.EqualTo( "ABCDEFGHIJKLMNOPQRSTUVXYZ" ) );
+		Assert.That( "123415647890".RemoveNonAlphanumericCharacters(), Is.EqualTo( "123415647890" ) );
+		Assert.That( "abcƒ±§╤ä".RemoveNonAlphanumericCharacters(), Is.EqualTo( "abc" ) );
+		Assert.That( "abc!@#$%^&*()_+{}|:\"<>?".RemoveNonAlphanumericCharacters(), Is.EqualTo( "abc" ) );
+		Assert.That( "".RemoveNonAlphanumericCharacters(), Is.Empty );
+		Assert.That( "   ".RemoveNonAlphanumericCharacters( preserveWhiteSpace: true ), Is.EqualTo( "   " ) );
+		Assert.That(
+			"  abcdefghijklmnopqrstuvxyz 123415647890  ".RemoveNonAlphanumericCharacters( preserveWhiteSpace: true ),
+			Is.EqualTo( "  abcdefghijklmnopqrstuvxyz 123415647890  " ) );
+		Assert.That(
+			"  abcdefghijklmnopqrstuvxyz 123415647890  \r\r\n".RemoveNonAlphanumericCharacters( preserveWhiteSpace: true ),
+			Is.EqualTo( "  abcdefghijklmnopqrstuvxyz 123415647890  \r\r\n" ) );
+	}
+}
