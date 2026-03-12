5dc467c8626305f2c41207f356f5cef53ff8dc79
William Gross <william@wgross.net>
2026-03-12 16:06:30 -0400

    Fixed AddValueToLine param in CsvFileWriter

--- Tewl/IO/CsvFileWriter.cs
+++ Tewl/IO/CsvFileWriter.cs
@@ -1,48 +1,45 @@
-﻿using System.IO;
-using JetBrains.Annotations;
+﻿namespace Tewl.IO;
 
-namespace Tewl.IO {
+/// <summary>
+/// Helps in writing data to a file in CSV format.
+/// </summary>
+[ PublicAPI ]
+public class CsvFileWriter: TextBasedTabularDataFileWriter {
+	private string line = "";
+
 	/// <summary>
-	/// Helps in writing data to a file in CSV format.
+	/// Clears the current line.  This does not affect the file at all, it simply undoes any
+	/// calls to AddValueToLine made since the last WriteCurrentLineToFile call.
 	/// </summary>
-	[ PublicAPI ]
-	public class CsvFileWriter: TextBasedTabularDataFileWriter {
-		private string line = "";
+	public void ClearLine() => line = "";
 
-		/// <summary>
-		/// Clears the current line.  This does not affect the file at all, it simply undoes any
-		/// calls to AddValueToLine made since the last WriteCurrentLineToFile call.
-		/// </summary>
-		public void ClearLine() => line = "";
+	/// <summary>
+	/// Adds the given value as a column on the current line.  Value may be null.  If
+	/// it is not null, val.ToString() determines what is added to the line.
+	/// </summary>
+	public void AddValueToLine( object? val ) {
+		if( val is null || val.ToString() == "" )
+			line += ",";
+		else
+			line += "\"" + val.ToString()!.Replace( "\"", "\"\"" ) + "\",";
+	}
 
-		/// <summary>
-		/// Adds the given value as a column on the current line.  Value may be null.  If
-		/// it is not null, val.ToString() determines what is added to the line.
-		/// </summary>
-		public void AddValueToLine( object val ) {
-			if( val == null || val.ToString() == "" )
-				line += ",";
-			else
-				line += "\"" + val.ToString().Replace( "\"", "\"\"" ) + "\",";
-		}
+	/// <summary>
+	/// Add several values to the current line.  Collection can be empty but not null.
+	/// </summary>
+	public void AddValuesToLine( params object[] values ) {
+		foreach( var value in values )
+			AddValueToLine( value );
+	}
 
-		/// <summary>
-		/// Add several values to the current line.  Collection can be empty but not null.
-		/// </summary>
-		public void AddValuesToLine( params object[] values ) {
-			foreach( var value in values )
-				AddValueToLine( value );
-		}
-
-		/// <summary>
-		/// Writes the current line to the file using the given open text writer.
-		/// This clears the current line after writing.
-		/// </summary>
-		public void WriteCurrentLineToFile( TextWriter writer ) {
-			line = line.TrimEnd( ',' );
-			writer.WriteLine( line );
-			writer.Flush();
-			line = "";
-		}
+	/// <summary>
+	/// Writes the current line to the file using the given open text writer.
+	/// This clears the current line after writing.
+	/// </summary>
+	public void WriteCurrentLineToFile( TextWriter writer ) {
+		line = line.TrimEnd( ',' );
+		writer.WriteLine( line );
+		writer.Flush();
+		line = "";
 	}
 }
